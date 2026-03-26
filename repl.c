2f9cb5af7fd02db2f8ed6b4f4c96835607ecbc42
Karol Jarzbek <183915723+karoljarzabek0@users.noreply.github.com>
2026-03-26 15:26:55 +0100

    added support for normal statements

--- repl.c
+++ repl.c
@@ -49,20 +49,84 @@ void close_input_buffer(InputBuffer* input_buffer) {
 	free(input_buffer);
 }
 
+typedef enum {
+	META_COMMAND_SUCCESS,
+	META_COMMAND_UNRECOGNISED_COMMAND
+} MetaCommandResult;
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+	if (strcmp(input_buffer->buffer, ".exit") == 0) {
+		exit(EXIT_SUCCESS);
+	} else {
+		return META_COMMAND_UNRECOGNISED_COMMAND;
+	}
+}
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+		StatementType type;
+} Statement;
+
+
+typedef enum {
+	PREPARE_SUCCESS,
+	PREPARE_UNRECOGNISED_STATEMENT
+} PrepareResult;
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+		if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+				statement->type = STATEMENT_INSERT;
+				return PREPARE_SUCCESS;
+		}
+		if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+				statement->type = STATEMENT_SELECT;
+				return PREPARE_SUCCESS;
+		}
+		return PREPARE_UNRECOGNISED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+		switch (statement->type) {
+				case (STATEMENT_INSERT):
+				printf("Insert!!!!.\n");
+				break;
+				case (STATEMENT_SELECT):
+				printf("Select!!!.\n");
+				break;
+		
+		}
+}
+
 int main() {
-	printf("Hello World!");
 	InputBuffer* input_buffer = new_input_buffer();
 
 	while(true) {
 		print_prompt();
 		read_input(input_buffer);
-
-		if (strcmp(input_buffer->buffer, ".exit") == 0) {
-			close_input_buffer(input_buffer);
-			exit(EXIT_SUCCESS);
+		
+		if (input_buffer->buffer[0] == '.') {
+			switch (do_meta_command(input_buffer)) {
+				case (META_COMMAND_SUCCESS):
+				continue;
+				case (META_COMMAND_UNRECOGNISED_COMMAND):
+				printf("Unrecognised command '%s'\n", input_buffer->buffer);
+				continue;
+			
+			}	
 		}
 
-	}
+		Statement statement;
+		switch (prepare_statement(input_buffer, &statement)) {
+				case (PREPARE_SUCCESS):
+				break;
+				case (PREPARE_UNRECOGNISED_STATEMENT):
+				printf("Unrecognised keyword: '%s'.\n",input_buffer->buffer);
+				continue;
+		
+		}
 
+		execute_statement(&statement);
+        printf("Executed.\n");
 	
-}
+		}}
